2d68fb0b5c695c7653200777ed4d1f144b93068a
vinogradovnick32 <vinogradovnick32@gmail.com>
2018-06-03 13:31:43 +0500

    add features

--- EducationalPratice_Task_1/Program.cs
+++ EducationalPratice_Task_1/Program.cs
@@ -35,9 +35,75 @@ namespace EducationalPratice_Task_1
             List<List<int>> All_States = new List<List<int>>();//Набор всех состояний
                 
             QueueParts.Enqueue(particles);//Добавляем частицы в очередь
-            All_States.Add();
+            All_States.Add(particles);
 
-            
+            while (QueueParts.Count>0)
+            {
+                List<int> temp = QueueParts.Dequeue();//Получаем частицы из очереди и выполняем действия над ними
+                Print(temp);
+                bool final = true;
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i; j < n; j++)
+                    {
+                        if (i == j)
+                        {
+                            if (temp[i] >= 2 && destr[i, i])
+                            {
+                                List<int> next = temp; //переходим к следующему состояни.
+                                next[i]--; //Уничтожаем частицы
+                                final = false;
+                                Print(next);
+                                if (!All_States.Contains(next)
+                                ) //Если данного варианта еще не было, то добавляем в очередь
+                                {
+                                    All_States.Add(next);
+                                    QueueParts.Enqueue(next);
+                                }
+                            }
+                        }
+                        else if (temp[i] > 0 && temp[j] > 0)
+                        {
+                            /* Частица i уничтожает частицу j */
+                            if (destr[i, j])
+                            {
+                                List<int> next = temp; //переходим к следующему состояни.
+                                next[j]--; //Уничтожаем частицы
+                                final = false;
+                                Print(next);
+                                if (!All_States.Contains(next)
+                                ) //Если данного варианта еще не было, то добавляем в очередь
+                                {
+                                    All_States.Add(next);
+                                    QueueParts.Enqueue(next);
+                                }
+                            }
+
+                            /* Частица j уничтожает частицу i */
+                            if (destr[j, i])
+                            {
+                                List<int> next = temp; //переходим к следующему состояни.
+                                Print(next);
+                                next[i]--; //Уничтожаем частицы
+                                final = false;
+                                if (!All_States.Contains(next)
+                                ) //Если данного варианта еще не было, то добавляем в очередь
+                                {
+                                    All_States.Add(next);
+                                    QueueParts.Enqueue(next);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (final)//Если финальное состояние, то добавляем конечный список
+                    FinalList.Add(temp);
+            }
+            Print(FinalList);
+            Print(All_States);
+            Console.ReadLine();
         }
 
         static void Print(List<List<int>> list)
