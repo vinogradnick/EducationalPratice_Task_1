83d99f4c51771a72aa0d4e8fd7d399509c4acc5d
vinogradovnick32 <vinogradovnick32@gmail.com>
2018-06-04 20:13:49 +0500

    all rewrite program

--- EducationalPratice_Task_1/Program.cs
+++ EducationalPratice_Task_1/Program.cs
@@ -1,125 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace EducationalPratice_Task_1
 {
     class Program
     {
+        private const int MAXN = 4;
+        private const int MAX_A = 3;
+        private static int n;
+       static  bool[,] destr = new bool[MAXN, MAXN];//Массив для удаления
+       static bool[,,,] dp =new bool[MAX_A,MAX_A,MAX_A,MAX_A];
+        static List<List<int>> ans = new List<List<int>>();
+
+        static void Bfs(int[] arr)
+        {
+            if(dp[arr[0], arr[1], arr[2], arr[3]]) // Если уже посещали вершину
+                return;
+            dp[arr[0], arr[1], arr[2], arr[3]] = true; // Вершина посещена
+            bool flag = false; // Проверка на конечное состояние
+            for (int i = 0; i < n; i++) // Бежим по взаимодействиям
+                if(arr[i] > 0) // Если частицы данного вида ещё есть
+                    for (int j = 0; j < n; j++) // Проверяем их на взаимодействие с другими
+
+                        if (destr[i, j] && arr[j] > 0 && (i != j || i == j && arr[j] > 1)
+                        ) // Если взаимодействие есть и есть что уничтожать
+
+                        {
+                            // Доп проверка (если частицы одного типа)
+                            arr[j]--;
+                            Bfs(arr);
+                            arr[j]++;
+                            flag = true;
+                        }
+
+            if (!flag) // Если состояние конечное, то добавляем
+            {
+                List<int> cur = new List<int>(n);
+                for (int i = 0; i < n; i++)
+                    cur.Add(arr[i]);
+                ans.Add(cur);
+            }
+        }
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());//Количество частиц
-            bool[,] destr = new bool[n, n];//Массив для удаления
-            List<int> particles = new List<int>(n);//Список частиц
+            n = Convert.ToInt32(Console.ReadLine());
+           int [] arr = new int[MAXN];
+            string part = Console.ReadLine();
+            int k = 0;
+
             for (int i = 0; i < n; i++)//Заполнение частиц
-                particles.Add(Convert.ToInt32(Console.ReadLine()));//Типы частиц
+            {
+                arr[i] = part[k] - '0';
+                k+=2;
+            }
 
-
-
+            for (int i = n; i < MAXN; i++)
+                arr[i] = 0;
 
             //Заполняем массив частицами
             for (int i = 0; i <n; i++)
             {
+                string pair = Console.ReadLine();
+                int jk = 0;
                 for (int j = 0; j <n; j++)
                 {
-                    int value = Convert.ToInt32(Console.ReadLine());
+                    int value = pair[jk] - '0';
+                    jk+=2;
                     destr[i, j] = (value != 0);
                 }
+            }
+
+           Bfs(arr);
+            Console.WriteLine(ans.Count);
+            foreach (var cur in ans)
+            {
+                foreach (var value in cur)
+                    Console.Write("{0} ", value);
                 Console.WriteLine();
             }
-            List<List<int>> FinalList = new List<List<int>>(n);
-            Queue<List<int>> QueueParts = new Queue<List<int>>(n);//Очередь частиц
-
-            List<List<int>> All_States = new List<List<int>>();//Набор всех состояний
-                
-            QueueParts.Enqueue(particles);//Добавляем частицы в очередь
-            All_States.Add(particles);
-
-            while (QueueParts.Count>0)
-            {
-                List<int> temp = QueueParts.Dequeue();//Получаем частицы из очереди и выполняем действия над ними
-                Print(temp);
-                bool final = true;
-
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = i; j < n; j++)
-                    {
-                        if (i == j)
-                        {
-                            if (temp[i] >= 2 && destr[i, i])
-                            {
-                                List<int> next = temp; //переходим к следующему состояни.
-                                next[i]--; //Уничтожаем частицы
-                                final = false;
-                                Print(next);
-                                if (!All_States.Contains(next)
-                                ) //Если данного варианта еще не было, то добавляем в очередь
-                                {
-                                    All_States.Add(next);
-                                    QueueParts.Enqueue(next);
-                                }
-                            }
-                        }
-                        else if (temp[i] > 0 && temp[j] > 0)
-                        {
-                            /* Частица i уничтожает частицу j */
-                            if (destr[i, j])
-                            {
-                                List<int> next = temp; //переходим к следующему состояни.
-                                next[j]--; //Уничтожаем частицы
-                                final = false;
-                                Print(next);
-                                if (!All_States.Contains(next)
-                                ) //Если данного варианта еще не было, то добавляем в очередь
-                                {
-                                    All_States.Add(next);
-                                    QueueParts.Enqueue(next);
-                                }
-                            }
-
-                            /* Частица j уничтожает частицу i */
-                            if (destr[j, i])
-                            {
-                                List<int> next = temp; //переходим к следующему состояни.
-                                Print(next);
-                                next[i]--; //Уничтожаем частицы
-                                final = false;
-                                if (!All_States.Contains(next)
-                                ) //Если данного варианта еще не было, то добавляем в очередь
-                                {
-                                    All_States.Add(next);
-                                    QueueParts.Enqueue(next);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (final)//Если финальное состояние, то добавляем конечный список
-                    FinalList.Add(temp);
-            }
-            Print(FinalList);
-            Print(All_States);
-            Console.ReadLine();
-        }
-
-        static void Print(List<List<int>> list)
-        {
-            foreach (var item in list)
-            {
-                foreach (var number in item)
-                    Console.Write($"{number} ");
-                Console.WriteLine();
-            }
-        }
-        static void Print(List<int> list)
-        {
-            foreach (var item in list)
-                Console.Write($"{item} ");
-            Console.WriteLine();
         }
     }
 }
